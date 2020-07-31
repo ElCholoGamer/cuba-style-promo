8b663c5e4e03822fcce854e0037967ed09852b98
ElCholoGamer <68793770+ElCholoGamer@users.noreply.github.com>
2020-07-30 19:47:08 -0500

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -1,11 +1,3 @@
 function redirect(page) {
     window.location.href = page;
 }
-
-fetch("./data.json")
-.then(function(resp) {
-    return resp.json();
-})
-.then(function(data) {
-    console.log(data);
-});
